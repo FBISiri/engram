edf736021f0e29e13c47a14d03d85af2b97c062c
Siri <masteragentsiri@gmail.com>
2026-08-27 09:54:09 +0800

    fix(ci): errcheck violations in evaporation test + merge Docker tags fix + align setup-go v6

--- pkg/server/evaporation_sweep_test.go
+++ pkg/server/evaporation_sweep_test.go
@@ -236,7 +236,7 @@ func TestSearchObservesEffectiveImportanceMetric(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer resp.Body.Close()
+	defer func() { _ = resp.Body.Close() }()
 	if resp.StatusCode != http.StatusOK {
 		t.Fatalf("status=%d, want 200", resp.StatusCode)
 	}
@@ -314,7 +314,7 @@ func TestPutRevivesEvaporationDeprecated(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer resp.Body.Close()
+	defer func() { _ = resp.Body.Close() }()
 	if resp.StatusCode != http.StatusOK {
 		t.Fatalf("status=%d, want 200", resp.StatusCode)
 	}
